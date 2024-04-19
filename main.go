78526e9ff087c2a51f92e307cc146caa6aa12d3c
Puttipong <puttipong.pao@gmail.com>
2024-04-19 19:56:03 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Puttipong1/assessment-tax/config"
 	"github.com/labstack/echo/v4"
@@ -13,5 +17,20 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(config.ServerConfig().Port()))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		if err := e.Start(config.ServerConfig().Port()); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
