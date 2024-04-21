c49489156972e303a4e1078d4d89498340d7e72e
Puttipong <puttipong.pao@gmail.com>
2024-04-21 22:33:10 +0700

    add calculate tax with wht

--- service/tax.go
+++ service/tax.go
@@ -18,16 +18,16 @@ func (service *TaxService) CalculateTax(tax request.Tax, deduction model.Deducti
 	if tax.TotalIncome < 150000.99 {
 		return response.TaxSummary{Tax: 0.0}
 	} else if tax.TotalIncome >= 150001 && tax.TotalIncome <= 500000.99 {
-		amount := decimal.NewFromFloat((tax.TotalIncome - 150000 - deduction.Personal) * 0.1)
+		amount := decimal.NewFromFloat((tax.TotalIncome - 150000 - tax.Wht - deduction.Personal) * 0.1)
 		return response.TaxSummary{Tax: amount.InexactFloat64()}
 	} else if tax.TotalIncome >= 500001 && tax.TotalIncome <= 1000000.99 {
-		amount := decimal.NewFromFloat((tax.TotalIncome - 500000 - deduction.Personal) * 0.15)
+		amount := decimal.NewFromFloat((tax.TotalIncome - 500000 - tax.Wht - deduction.Personal) * 0.15)
 		return response.TaxSummary{Tax: amount.InexactFloat64()}
 	} else if tax.TotalIncome >= 1000001 && tax.TotalIncome <= 2000000.99 {
-		amount := decimal.NewFromFloat((tax.TotalIncome - 150000 - deduction.Personal) * 0.2)
+		amount := decimal.NewFromFloat((tax.TotalIncome - 150000 - tax.Wht - deduction.Personal) * 0.2)
 		return response.TaxSummary{Tax: amount.InexactFloat64()}
 	} else {
-		amount := decimal.NewFromFloat((tax.TotalIncome - 2000000 - deduction.Personal) * 0.35)
+		amount := decimal.NewFromFloat((tax.TotalIncome - 2000000 - tax.Wht - deduction.Personal) * 0.35)
 		return response.TaxSummary{Tax: amount.InexactFloat64()}
 	}
 }
