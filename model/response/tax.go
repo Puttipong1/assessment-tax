47bf540ea9d984ff710f97095464df5eecf6cc21
Puttipong <puttipong.pao@gmail.com>
2024-04-22 22:49:47 +0700

    add taxlevel response

--- model/response/tax.go
+++ model/response/tax.go
@@ -1,6 +1,12 @@
 package response
 
 type TaxSummary struct {
-	Tax       float64  `json:"tax"`
-	TaxRefund *float64 `json:"taxRefund,omitempty"`
+	Tax       float64    `json:"tax"`
+	TaxRefund *float64   `json:"taxRefund,omitempty"`
+	TaxLevel  []TaxLevel `json:"taxLevel"`
+}
+
+type TaxLevel struct {
+	Level string  `json:"level"`
+	Tax   float64 `json:"tax"`
 }
