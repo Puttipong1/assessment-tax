a106285267d0ebd1f539a5cbb107b951fc58332a
Puttipong <puttipong.pao@gmail.com>
2024-04-25 20:12:06 +0700

    update docker compose and script database

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS deductions
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    type character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    amount numeric NOT NULL,
+    CONSTRAINT deductions_pkey PRIMARY KEY (id),
+    CONSTRAINT deductions_type UNIQUE (type)
+)
+
+INSERT INTO "deductions" ("type", "amount") VALUES ('personal', 50000.0)  ON conflict ("type") do nothing; 
+INSERT INTO "deductions" ("type", "amount") VALUES ('k-receipt', 50000.0) ON conflict ("type") do nothing;
